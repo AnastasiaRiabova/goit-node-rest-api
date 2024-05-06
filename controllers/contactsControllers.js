f722669744d65880ac74d5df178c78ced3e64a8e
AnastasiaRiabova <riabova321@gmail.com>
2024-05-06 20:39:01 +0200

    add first tasks

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,8 +1,43 @@
-import contactsService from "../services/contactsServices.js";
+import  {listContacts, getContactById, removeContact, addContact} from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res, next) => {
+  try {
+      const contacts = await listContacts()
+      res.json({
+          status: 'success',
+          code: 200,
+          data: {
+              contacts
+          },
+      });
+  }
+  catch (error) {
+      next(error)
+  }
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res, next) => {
+
+    try {
+        const {id} = req.params
+        const contact = await getContactById(id)
+        if (contact) {
+          return res.json({
+                status: 'success',
+                code: 200,
+                data: {
+                    contact
+                },
+            });
+        }
+        throw HttpError(404, 'not found')
+    }
+    catch (error) {
+        next(error)
+    }
+
+};
 
 export const deleteContact = (req, res) => {};
 
