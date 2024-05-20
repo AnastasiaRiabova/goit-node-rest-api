1e4f4bde37a0b1197c5593a346b8d2375392456a
AnastasiaRiabova <riabova321@gmail.com>
2024-05-20 16:13:33 +0200

    add homework

---
+++ schemas/userSchema.js
@@ -0,0 +1,33 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "User must have username"],
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+const User = model("user", userSchema);
+
+export default User;
